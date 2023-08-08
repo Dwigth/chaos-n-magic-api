b0d42f4f594e1bbc19846c2175066277c5d96369
Dwigth Astacio <dwigth32@outlook.es>
2023-08-08 16:08:18 -0600

    feat(hotfix): apply rate-limit for all modules

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerConfig } from 'app/config/enums/config.enum';
 import { HeroSheetModule } from 'app/hero-sheet/hero-sheet.module';
 
 @Module({
@@ -8,8 +11,17 @@ import { HeroSheetModule } from 'app/hero-sheet/hero-sheet.module';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     HeroSheetModule,
+    ThrottlerModule.forRoot({
+      ttl: ThrottlerConfig.TTL,
+      limit: ThrottlerConfig.LIMIT,
+    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
