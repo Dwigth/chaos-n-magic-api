0249b92955c8f51bf9c0146429573d01272811e6
Dwigth Astacio <dwigth32@outlook.es>
2023-08-30 15:37:31 -0600

    fix(main): enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from 'main.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Chaos n Magic')
     .setDescription('The chaos-n-magic API description')
